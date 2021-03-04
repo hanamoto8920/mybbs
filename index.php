fb57522b108173aba7449184c57230d58d0eb319
花本敬太 <hanamotokeita@hanamotokeitanoiMac.local>
2021-03-04 19:47:59 +0900

    first commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<html lang="ja">
+<head>
+  <meta charset="utd-8">
+  <title>MyBBS</title>
+</head>
+<body>
+  <h1>MyBBS(簡易掲示板)</h1>
+  <form action="">
+  メッセージ: <input type="text" name="message">
+  投稿者名: <input type="text" name="user">
+  <input type="submit" value="投稿">
+  </form>
+  <h2>投稿一覧(０件)</h2>
+  <ul>
+    <li>投稿はまだありません。</li>
+  </ul>
+</body>
+</html>
